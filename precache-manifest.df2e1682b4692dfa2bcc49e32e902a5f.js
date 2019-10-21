141e1a5a99a2d4338c8cd3cb00dbc23468b900d2
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-10-21 14:35:36 -0300

    Updates

---
+++ precache-manifest.df2e1682b4692dfa2bcc49e32e902a5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1bea2ab1d04190c33032df81409299",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "c1d013c762b650b069ba",
+    "url": "/ecommerce/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "43c39d60ace2cf4ff912",
+    "url": "/ecommerce/static/css/main.5dbc73f7.chunk.css"
+  },
+  {
+    "revision": "c1d013c762b650b069ba",
+    "url": "/ecommerce/static/js/2.b94b5aec.chunk.js"
+  },
+  {
+    "revision": "43c39d60ace2cf4ff912",
+    "url": "/ecommerce/static/js/main.4f22ad07.chunk.js"
+  },
+  {
+    "revision": "9aa1de8f5f4220762f76",
+    "url": "/ecommerce/static/js/runtime-main.234f0e98.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce/static/media/shopping-bag.30f07071.svg"
+  }
+]);
